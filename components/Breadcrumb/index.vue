f54025e082c023c4d008f0ea93eb9c1120ad6475
clayja <clayja@163.com>
2021-07-21 17:35:34 +0400

    feat: detail

---
+++ components/Breadcrumb/index.vue
@@ -0,0 +1,64 @@
+<template>
+  <ol class="breadcrumb-nav breadcrumb">
+    <i class="icon iconfont icon-icon_wechat">&#xe600;</i>
+    <li v-for="(item, i) in routeList" :key="i">
+      <nuxt-link
+        :class="`${currentPath === item.path ? 'active' : 'black'}`"
+        :to="item.path"
+      >{{ item.name }}</nuxt-link>
+    </li>
+  </ol>
+</template>
+<script lang="ts">
+export default {
+  props: {
+    data: {
+      type: Array,
+      default: []
+    }
+  },
+  data() {
+    const path = this.$route.path
+    console.log('path', path)
+    return {
+      currentPath: path,
+      routeList: this.data
+    }
+  },
+}
+</script>
+<style lang="less" scoped>
+.breadcrumb-nav {
+  height: 63px;
+  line-height: 63px;
+  background: #fff;
+  margin-bottom: 0;
+  padding: 8px 150px;
+  a {
+    font-size: 13px;
+    font-weight: 400;
+    color: #9A9A9A;
+  }
+  .active {
+    a {
+      color: #354194;
+    }
+  }
+  .icon {
+    color: #354194;
+    margin-right: 10px;
+  }
+  @media only screen and (max-width: 760px) {
+    height: 63px;
+    line-height: 63px;
+    a {
+      font-size: 13px;
+    }
+    .icon {
+      margin-right: 10px;
+    }
+  }
+}
+
+
+</style>
---
+++ pages/news/detail.vue
@@ -0,0 +1,198 @@
+<template>
+  <div class="index-wrapper">
+    <Header />
+    <div class="breadcrumb-wrapper">
+      <Breadcrumb :data="routes" />
+    </div>
+    <div class="wrapper">
+      <div class="title-wrapper">
+        <div class="title">【原创】英特科技亮相“热泵变频与控制方法技术论坛”</div>
+        <div class="description">
+          <div class="date">2021-03-13 16:06:06</div>
+          <div class="author">蔡文广</div>
+          <div class="source">来源： 热泵产业资讯</div>
+        </div>
+      </div>
+      <div class="pager-wrapper">
+        <div class="prev step disabled"><i class="iconfont icon">&#xe608;</i>上一页</div>
+        <div class="next step">下一页<i class="iconfont icon">&#xe60a;</i></div>
+      </div>
+    </div>
+    <ContactUs />
+    <Footer />
+  </div>
+</template>
+
+<script lang="ts">
+import Breadcrumb from '@/components/Breadcrumb/index.vue'
+export default {
+  data() {
+    return {
+      routes: [
+        {
+          name: '英特',
+          path: '/',
+        },
+        {
+          name: '新闻资讯',
+          path: '/news',
+        },
+        {
+          name: '正文',
+          path: '/news/detail',
+        },
+      ],
+    }
+  },
+  methods: {
+  },
+  components: {
+    Breadcrumb,
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.index-wrapper {
+  background:#F7F7F7;
+  .wrapper {
+    margin: 48px 150px 96px 150px;
+    background: #fff;
+    padding: 0 64px;
+    text-align: center;
+    .title-wrapper {
+      padding-top: 64px;
+      padding-bottom: 12px;
+      border-bottom: 1px solid #E5E5E5;
+      .title {
+        font-size: 20px;
+        font-family: PingFangSC-Medium, PingFang SC;
+        font-weight: 500;
+        color: #474747;
+        line-height: 28px;
+        margin-bottom: 24px;
+      }
+      .description {
+        font-size: 14px;
+        font-family: PingFangSC-Regular, PingFang SC;
+        font-weight: 400;
+        color: #A6AAB1;
+        line-height: 24px;
+        display: inline-flex;
+        & > div  {
+          margin-left: 1em;
+        }
+      }
+    }
+  }
+  .pager-wrapper {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    font-size: 20px;
+    font-family: PingFangSC-Regular, PingFang SC;
+    font-weight: 400;
+    color: #3E3E3E;
+    line-height: 28px;
+    letter-spacing: 1px;
+    padding: 48px 0;
+    .icon {
+      font-size: 38px;
+    }
+    .disabled {
+      color: #9A9A9A;
+    }
+    .step {
+      cursor: pointer;
+      &:hover {
+        color: #354194;
+      }
+    }
+    .prev {
+      display: inline-flex;
+      align-items: center;
+      .icon {
+        margin-right: 16px;
+      }
+    }
+    .next {
+      display: inline-flex;
+      align-items: center;
+      .icon {
+        margin-left: 16px;
+      }
+    }
+  }
+  @media only screen and (max-width: 760px) {
+    .wrapper {
+        margin: 16px;
+        padding: 0 16px;
+      .title-wrapper {
+        padding-top: 32px;
+        padding-bottom: 12px;
+        border-bottom: 1px solid #E5E5E5;
+        .title {
+          font-size: 20px;
+          font-family: PingFangSC-Medium, PingFang SC;
+          font-weight: 500;
+          color: #474747;
+          line-height: 28px;
+          margin-bottom: 24px;
+        }
+        .description {
+          font-size: 14px;
+          font-family: PingFangSC-Regular, PingFang SC;
+          font-weight: 400;
+          color: #A6AAB1;
+          line-height: 24px;
+          display: inline-flex;
+          flex-wrap: wrap;
+          align-items: center;
+          justify-content: center;
+          & > div {
+            margin-left: 1em;
+          }
+        }
+      }
+    }
+    .pager-wrapper {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      font-size: 20px;
+      font-family: PingFangSC-Regular, PingFang SC;
+      font-weight: 400;
+      color: #3E3E3E;
+      line-height: 28px;
+      letter-spacing: 1px;
+      padding: 16px 0;
+      .icon {
+        font-size: 38px;
+      }
+      .disabled {
+        color: #9A9A9A;
+      }
+      .step {
+        cursor: pointer;
+        &:hover {
+          color: #354194;
+        }
+      }
+      .prev {
+        display: inline-flex;
+        align-items: center;
+        .icon {
+          margin-right: 16px;
+        }
+      }
+      .next {
+        display: inline-flex;
+        align-items: center;
+        .icon {
+          margin-left: 16px;
+        }
+      }
+    }
+  }
+}
+</style>
