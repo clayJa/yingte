a37add2eaf245d264ba405c182202b57b4d88a68
clayja <clayja@163.com>
2021-08-03 16:29:56 +0400

    fix: 环境

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-docker build --tag "$1" .
+docker build --tag "lingxiangyang/$1" .
 array=(${1//\// })
 len=${#array[*]}
 filename=${array[len-1]}
-docker save -o "./$filename.tar" $1
-docker image rm $1
+#docker save -o "./$filename.tar" $1
+# docker image rm $1
