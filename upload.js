d6f4e809cfefaa0b9768f9f049a833cd3005ed00
Yang Guo <gy812337967@outlook.com>
2021-08-08 19:08:14 +0800

    并发限制调整

--- upload.js
+++ upload.js
@@ -37,7 +37,8 @@ function addFileToOSSSync(src, dist, isDir) {
     let st = fs.statSync(_src);
     // 判断是否为文件
     if (st.isFile() && dist !== "LICENSES`") {
-      let arr = dist.split(',');
+      let dist_str = dist + '';
+      let arr = dist_str.split(',');
 
       putOSS(_src, arr[arr.length - 1] + `/${doc}`);//如果是文件夹下文件，文件名为 fonts/文件名
 
