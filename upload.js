19a3f14e4f4967aff62f08e526961778e6b8ffd1
Yang Guo <gy812337967@outlook.com>
2021-08-08 16:00:31 +0800

    并发限制调整

--- upload.js
+++ upload.js
@@ -1,4 +1,3 @@
-
 const qiniu = require("qiniu");
 const async = require('async');
 const fs = require("fs");
@@ -14,7 +13,7 @@ var options = {
   scope: 'intweb',
 };
 var putPolicy = new qiniu.rs.PutPolicy(options);
-var uploadToken=putPolicy.uploadToken(mac);
+var uploadToken = putPolicy.uploadToken(mac);
 
 var config = new qiniu.conf.Config();
 // 空间对应的机房
@@ -30,38 +29,50 @@ config.useCdnDomain = false;
  * @param {string} dist oos文件夹名
  * @param {boolean} isDir 是否为文件夹下文件
  */
-async function addFileToOSSSync(src, dist,isDir) {
+function addFileToOSSSync(src, dist, isDir) {
   let docs = fs.readdirSync(src);
-  docs.forEach(function(doc) {
-    let _src = src +'/'+ doc,
-      _dist = dist +'/'+ doc;
+  docs.forEach(function (doc) {
+    let _src = src + '/' + doc,
+      _dist = dist + '/' + doc;
     let st = fs.statSync(_src);
     // 判断是否为文件
     if (st.isFile() && dist !== "LICENSES`") {
 
-      putOSS(_src, !isDir?doc: dist+`/${doc}`);//如果是文件夹下文件，文件名为 fonts/文件名
+      putOSS(_src, !isDir ? doc : dist + `/${doc}`);//如果是文件夹下文件，文件名为 fonts/文件名
 
     }
     // 如果是目录则递归调用自身
     else if (st.isDirectory()) {
-      addFileToOSSSync(_src, _dist,true);
+      addFileToOSSSync(_src, _dist, true);
     }
   });
 }
 
-async function addFileToOSSSyncV2(src, dist,isDir){
+var taskTotal = 0;
+var syncCount = 0;
+
+async function addFileToOSSSyncV2(src, dist, isDir) {
   let docs = fs.readdirSync(src);
+  taskTotal = docs.length;
+
+  task(docs, src, dist, isDir);
+
+
+}
+
+function task(docs, src, dist, isDir) {
   async.mapLimit(docs, 5, function (doc) {
-    let _src = src +'/'+ doc,
-      _dist = dist +'/'+ doc;
+    syncCount++;
+    let _src = src + '/' + doc,
+      _dist = dist + '/' + doc;
     let st = fs.statSync(_src);
     // 判断是否为文件
     if (st.isFile() && dist !== "LICENSES`") {
-      putOSS(_src, !isDir?doc: dist+`/${doc}`);//如果是文件夹下文件，文件名为 fonts/文件名
+      putOSS(_src, !isDir ? doc : dist + `/${doc}`);//如果是文件夹下文件，文件名为 fonts/文件名
     }
     // 如果是目录则递归调用自身
     else if (st.isDirectory()) {
-      addFileToOSSSyncV2(_src, _dist,true);
+      addFileToOSSSyncV2(_src, _dist, true);
     }
   }, function (error, result) {
     console.log("result: ")
@@ -77,13 +88,12 @@ async function putOSS(src, dist) {
     var localFile = src;
     var formUploader = new qiniu.form_up.FormUploader(config);
     var putExtra = new qiniu.form_up.PutExtra();
-    var key=dist;
-
+    var key = dist;
 
 
     // 文件上传
-    await formUploader.putFile(uploadToken, key, localFile, putExtra, function(respErr,
-                                                                               respBody, respInfo) {
+    await formUploader.putFile(uploadToken, key, localFile, putExtra, function (respErr,
+                                                                                respBody, respInfo) {
       if (respErr) {
         throw respErr;
       }
@@ -98,17 +108,19 @@ async function putOSS(src, dist) {
     console.log("上传失败".e);
   }
 }
+
 /**
  *上传文件启动
  */
 async function upFile() {
   try {
-    let src = PUBLIC_PATH+".nuxt/dist/client";
+    let src = PUBLIC_PATH + ".nuxt/dist/client";
     let docs = fs.readdirSync(src);
     //await addFileToOSSSync(src, docs);
-    addFileToOSSSync(src,docs);
+    addFileToOSSSync(src, docs);
   } catch (err) {
     console.log("上传oss失败", err);
   }
 }
+
 upFile();
