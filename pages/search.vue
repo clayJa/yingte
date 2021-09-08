c5fc9d7f4c8ad34c2fbc16752c67946d791af9de
clayja <clayja@163.com>
2021-09-08 12:19:33 +0400

    fix

--- pages/search.vue
+++ pages/search.vue
@@ -180,8 +180,11 @@ export default {
         margin-right: 32px;
         img {
           width: 100%;
+          height: 100%;
           max-width: 100%;
           max-height: 100%;
+          object-fit: cover;
+          border-radius: 4px;
         }
       }
       .content-wrapper {
